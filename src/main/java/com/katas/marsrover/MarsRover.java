03683d05b36e12b6153274cf9db23ab11e058bd1
devtoni <devtonirm@gmail.com>
2018-11-01 12:50:59 +0100

    test(marsrover): green return constant

--- src/main/java/com/katas/marsrover/MarsRover.java
+++ src/main/java/com/katas/marsrover/MarsRover.java
@@ -1,4 +1,7 @@
 package com.katas.marsrover;
 
 public class MarsRover {
+    public String execute(String commands) {
+        return "0:0:W";
+    }
 }
--- src/test/java/com/katas/marsrover/MarsRoverShould.java
+++ src/test/java/com/katas/marsrover/MarsRoverShould.java
@@ -1,4 +1,14 @@
 package com.katas.marsrover;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MarsRoverShould {
+    @Test
+    public void rotate_to_left_when_commands_are_executed() {
+        MarsRover marsRover = new MarsRover();
+
+        assertEquals(marsRover.execute("L"), "0:0:W");
+    }
 }
